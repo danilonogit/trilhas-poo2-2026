3fe7477c2ed477e3d8a3e1ea43a96df629975cf2
Danilo Vitor <danilo.vitor@alunos.ifsuldeminas.edu.br>
2026-02-23 16:07:48 -0300

    Classe Cliente adicionada

--- src/main/java/br/edu/tds/projetopoo/ProjetoPOO.java
+++ src/main/java/br/edu/tds/projetopoo/ProjetoPOO.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.edu.tds.projetopoo;
-
-/**
- *
- * @author aluno
- */
 public class ProjetoPOO {
 
     public static void main(String[] args) {
