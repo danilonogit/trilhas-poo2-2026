6df1b6451cc603758194191d76af007d20330b8e
Danilo Vitor <danilo.vitor@alunos.ifsuldeminas.edu.br>
2026-02-23 17:13:09 -0300

    Criando a classe main e testando

---
+++ src/main/java/br/edu/tds/projetopoo/Main.java
@@ -0,0 +1,14 @@
+package br.edu.tds.projetopoo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Testando objetos da classe Cliente");
+        //Usando a classe Cliente
+        Cliente c1 = new Cliente("Davi",19,"123456789");
+        c1.imprimeCliente();
+    } 
+}
--- src/main/java/br/edu/tds/projetopoo/ProjetoPOO.java
+++ src/main/java/br/edu/tds/projetopoo/ProjetoPOO.java
@@ -4,5 +4,7 @@ public class ProjetoPOO {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Primeiro Código POOII");
+        
+        
     }
 }
